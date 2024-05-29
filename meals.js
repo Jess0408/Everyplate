5675e70c4c5f5e22f3de67a5e7950157b529733d
Jessica Yu <huijiey@student.unimelb.edu.au>
2024-05-29 17:25:54 +1000

    modal

--- meals.js
+++ meals.js
@@ -1,7 +1,6 @@
 
 // Get the modal
 var modal = document.getElementById('id01');
-
 var home_icon = document.getElementsByClassName('menu-icon');
 
 home_icon.onclick = function() {
@@ -12,5 +11,13 @@ home_icon.onclick = function() {
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  }
+}
+
+var p = document.getElementsByClassName('preference');
+
+window.onclick = function(event) {
+  if (event.target == p) {
+    p.style.display = "none";
   }
 }
